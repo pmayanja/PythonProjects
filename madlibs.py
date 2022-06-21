e859c9d1e9664bacd7f9eef60fe88ef39aa8f931
pmayanja <37284527+pmayanja@users.noreply.github.com>
2022-06-22 02:50:18 +0300

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+# Learning string concatenation
+# Basic mad libs game
+
+colour = input("Enter a colour: ")
+plural_noun2 = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+
+
+print("Roses are " + colour)
+print(plural_noun2 + " are blue")
+print("I love " + celebrity)
